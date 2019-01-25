bb5a790047f0e22bfef1a29c441f5ee843a0f246
aisav <artur.isaverdyan@gmail.com>
2019-01-25 15:00:12 +0400

    mirage addition to app

---
+++ mirage/fixtures/products.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'Salt', quantity: 4, price: 100},
+  {id: 2, name: 'Bread', quantity: 3, price: 200},
+  {id: 3, name: 'Jermuk', quantity: 5, price: 150},
+  {id: 4, name: 'Coca Cola', quantity: 5, price: 150},
+];
---
+++ mirage/fixtures/shops.js
@@ -0,0 +1,5 @@
+export default [
+    {id: 1, name: 'Shop 1', productIds:[1,2]},
+    {id: 2, name: 'Sas', productIds:[3, 4]},
+    {id: 3, name: 'Yerevan City', productIds:[1,3]}
+];
